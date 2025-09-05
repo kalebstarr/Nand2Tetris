dbecf2938e38ef9368eedaa6148607e110ab440d
Kaleb <kaleb.starr@proton.me>
2025-09-05 17:36:18 +0200

    Add initial file reading

--- 06/src/main.zig
+++ 06/src/main.zig
@@ -11,5 +11,17 @@ pub fn main() !void {
     const allocator = gpa.allocator();
 
     const argv = try std.process.argsAlloc(allocator);
-    std.debug.print("{s}", .{argv[0]});
+    defer std.process.argsFree(allocator, argv);
+    // TODO: Improve error output message
+    std.debug.assert(argv.len == 2);
+
+    const file = try std.fs.cwd().openFile(argv[1], .{});
+    defer file.close();
+    const buffer = try allocator.alloc(u8, 32 * 1024);
+    defer allocator.free(buffer);
+    var file_reader = file.reader(buffer);
+    const file_size = try file_reader.getSize();
+
+    const file_contents = try file_reader.interface.readAlloc(allocator, file_size);
+    defer allocator.free(file_contents);
 }
