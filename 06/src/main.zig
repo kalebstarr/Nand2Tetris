23f65dc257df3f880633fab97a48771ffaa8fa66
Kaleb <kaleb.starr@proton.me>
2025-09-07 17:19:14 +0200

    Extract SymbolTable

--- 06/src/main.zig
+++ 06/src/main.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const testing = std.testing;
 const Parser = @import("parser.zig").Parser;
+const Code = @import("code.zig").Code;
+const SymbolTable = @import("symbol_table.zig");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
@@ -38,38 +40,14 @@ pub fn main() !void {
     var parser = Parser.initFromLines(allocator, lines);
     defer parser.deinit();
 
-    var symbol_table = try initSymbolTable(allocator);
+    _ = try parser.instructionType();
+    _ = try parser.jump();
+
+    var code = try Code.init(allocator);
+    defer code.deinit();
+
+    var symbol_table = try SymbolTable.init(allocator);
     defer symbol_table.deinit();
-}
-
-fn initSymbolTable(allocator: std.mem.Allocator) !std.StringHashMap(i16) {
-    var hash_map = std.StringHashMap(i16).init(allocator);
-
-    try hash_map.put("R0", 0);
-    try hash_map.put("R1", 1);
-    try hash_map.put("R2", 2);
-    try hash_map.put("R3", 3);
-    try hash_map.put("R4", 4);
-    try hash_map.put("R5", 5);
-    try hash_map.put("R6", 6);
-    try hash_map.put("R7", 7);
-    try hash_map.put("R8", 8);
-    try hash_map.put("R9", 9);
-    try hash_map.put("R10", 10);
-    try hash_map.put("R11", 11);
-    try hash_map.put("R12", 12);
-    try hash_map.put("R13", 13);
-    try hash_map.put("R14", 14);
-    try hash_map.put("R15", 15);
-    try hash_map.put("SCREEN", 16384);
-    try hash_map.put("KBD", 24576);
-    try hash_map.put("SP", 0);
-    try hash_map.put("LCL", 1);
-    try hash_map.put("ARG", 2);
-    try hash_map.put("THIS", 3);
-    try hash_map.put("THAT", 4);
-
-    return hash_map;
 }
 
 fn readFile(allocator: std.mem.Allocator, file_name: []const u8) !std.ArrayList([]const u8) {
---
+++ 06/src/symbol_table.zig
@@ -0,0 +1,45 @@
+const std = @import("std");
+
+pub const SymbolTable = struct {
+    allocator: std.mem.Allocator,
+    table: std.StringHashMap(i16),
+    new_symbol_index: u16,
+
+    pub fn init(allocator: std.mem.Allocator) !SymbolTable {
+        return SymbolTable{ .allocator = allocator, .table = try initSymbolTable(allocator), .new_symbol_index = 16 };
+    }
+
+    pub fn deinit(self: *SymbolTable) void {
+        self.table.deinit();
+    }
+
+    fn initSymbolTable(allocator: std.mem.Allocator) !std.StringHashMap(i16) {
+        var table = std.StringHashMap(i16).init(allocator);
+
+        try table.put("R0", 0);
+        try table.put("R1", 1);
+        try table.put("R2", 2);
+        try table.put("R3", 3);
+        try table.put("R4", 4);
+        try table.put("R5", 5);
+        try table.put("R6", 6);
+        try table.put("R7", 7);
+        try table.put("R8", 8);
+        try table.put("R9", 9);
+        try table.put("R10", 10);
+        try table.put("R11", 11);
+        try table.put("R12", 12);
+        try table.put("R13", 13);
+        try table.put("R14", 14);
+        try table.put("R15", 15);
+        try table.put("SCREEN", 16384);
+        try table.put("KBD", 24576);
+        try table.put("SP", 0);
+        try table.put("LCL", 1);
+        try table.put("ARG", 2);
+        try table.put("THIS", 3);
+        try table.put("THAT", 4);
+
+        return table;
+    }
+};
