a30a7e6dc87ccab7ee278ce2d54a193f026b8d35
Kaleb <kaleb.starr@proton.me>
2025-09-11 19:57:05 +0200

    Add initial validation

--- 07/src/main.zig
+++ 07/src/main.zig
@@ -1,3 +1,27 @@
 const std = @import("std");
 
-pub fn main() !void {}
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{});
+    defer {
+        const gpa_status = gpa.deinit();
+        if (gpa_status == .leak) {
+            std.testing.expect(false) catch @panic("TEST FAIL");
+        }
+    }
+    const allocator = gpa.allocator();
+
+    const argv = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, argv);
+    if (argv.len != 2) {
+        std.debug.print("Invalid number of arguments", .{});
+        return;
+    }
+
+    const file_name = argv[1];
+
+    if (!std.mem.endsWith(u8, file_name, ".vm")) {
+        std.debug.print("Invalid file name", .{});
+        return;
+    }
+}
+
